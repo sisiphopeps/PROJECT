96073a89b74542174c32c711b12790dfb6bf8e55
unknown <sisiphopepengweni@gmail.com>
2023-09-01 15:16:33 +0200

    second commit

---
+++ frontend/src/components/NavbarComp.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Sisipho's<br>Vogue Vault</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <router-link class="nav-link" to="/">HOME</router-link>
+
+              <router-link class="nav-link" to="/about">ABOUT</router-link>
+
+              <router-link class="nav-link" to="/products">PRODUCTS</router-link>
+
+              <router-link class="nav-link" to="/contact">CONTACT</router-link>
+
+              <router-link class="nav-link" to="/admin">ADMIN</router-link>
+
+              <router-link class="nav-link" to="/login">LOGIN</router-link>
+
+              <router-link class="nav-link" to="/wishlist">WISHLIST</router-link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 10px;
+  background-color: #F4C2C2 !important;
+  color: white !important;
+}
+
+.navbar-nav {
+  font-weight: bold;
+ color:white !important;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,105 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="https://i.postimg.cc/hj3x0095/Screenshot-2023-08-28-095221.png" alt="" class="logo">
+    <div>
+      <h2>FEATURED PRODUCTS</h2>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/1XgRdvPJ/designer-purse-500x500.webp" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Handbag</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/13Dh4LJp/ferragamo-orange-bag.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Handbag</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/WzQQCxww/30-H1-GGRS2-L-0001-1.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Handbag</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/ydrM3B98/MK7378-0710-1.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Watch</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/hPKwrpbB/MK6958-0710-1.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Watch</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/DyppnFsC/MK6993-0040-1.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Watch</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/Fs4ZFt67/versace-BLACK-Round-frame-Sunglasses-Designer-Sunglasses-Lenses.webp" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Sunglasses</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/fL5xBrLp/2c5d288e-a750-48a6-b244-0fd76a8eb417.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Sunglasses</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="https://i.postimg.cc/mk5fF9cq/images-3.jpg" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Sunglasses</p>
+        <a href="#" class="btn btn-primary">Add to wishlist</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
+
+<style scoped>
+.logo{
+  width:100%;
+}
+</style>
