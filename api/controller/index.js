cb47d9cc71a1d658e5997070c80e86a1f753201b
unknown <sisiphopepengweni@gmail.com>
2023-09-13 11:29:30 +0200

    all commits

--- api/controller/index.js
+++ api/controller/index.js
@@ -15,10 +15,6 @@ routes.post('/register',bodyParser.json(),
 (req, res)=>{
     users.register(req, res)
 })
-routes.put('/user/:id', bodyParser.json(),
- (req, res)=>{
-    users.updateUser( req,res)
-})
 routes.patch('/user/:id', bodyParser.json(),
  (req, res)=>{
     users.updateUser(req,res)
@@ -36,7 +32,7 @@ routes.get('/products', (req, res) => {
     products.getProducts(req, res)
 })
 // Get a single product route
-routes.get('/products/:id', (req, res) => {
+routes.get('/product/:id', (req, res) => {
     products.getProduct(req, res)
 })
 // Add a product route
@@ -44,33 +40,14 @@ routes.post('/add-product', bodyParser.json(), (req, res) => {
     products.addProduct(req, res)
 })
 // Update a single route route
-routes.put('/products/:id', bodyParser.json(), (req, res) => {
-    products.updateProduct(req, res)
-})
-routes.get('/products', (req, res) => {
-    products.getProducts(req, res)
-})
-// Get a single product route
-routes.get('/products/:id', (req, res) => {
-    products.getProduct(req, res)
-})
-// Add a product route
-routes.post('/add-product', bodyParser.json(), (req, res) => {
-    products.addProduct(req, res)
-})
-// Update a single route route
-routes.put('/products/:id', bodyParser.json(), (req, res) => {
-    products.updateProduct(req, res)
-})
-routes.patch('/products/:id', bodyParser.json(), (req, res) => {
+// routes.put('/product/:id', bodyParser.json(), (req, res) => {
+//     products.updateProduct(req, res)
+// })
+routes.patch('/updateProduct/:id', bodyParser.json(), (req, res) => {
     products.updateProduct(req, res)
 })
 // Delete a product route
-routes.delete('/products/:id', (req, res) => {
-    products.deleteProduct(req, res)
-})
-// Delete a product route
-routes.delete('/products/:id', (req, res) => {
+routes.delete('/deleteProduct/:id', (req, res) => {
     products.deleteProduct(req, res)
 })
 module.exports = {
